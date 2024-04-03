2b1c1373587305a794482ce39e56a61f0071cca7
mrigendra <mrigendrasingh845@gmail.com>
2024-04-03 23:06:25 +0530

    commit

---
+++ script.js
@@ -0,0 +1,26 @@
+
+
+var form = document.querySelector('form')
+form.addEventListener('submit',function(e)
+{e.preventDefault();
+
+const height =parseInt( document.querySelector('#height').value)
+const weight =parseInt( document.querySelector('#weight').value)
+const results =  document.querySelector('#results')
+
+
+if(height === ' ' || height< 0    ||  isNaN(height)){
+    results.innerHTML = `please give a valid height ${height}`;
+}
+
+else if 
+    
+(weight === ' ' || weight< 0    ||  isNaN(weight)){
+    results.innerHTML = `please give a valid weight ${weight}`;
+}
+
+else{
+const bmi = (weight/((height* height)/10000)).toFixed(3)
+
+results.innerHTML =  `<span>${bmi}</span>`};
+});
